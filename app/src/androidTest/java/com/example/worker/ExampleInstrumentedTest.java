87a18b53ede7b1da289ffb188b732b280afd1c0d
pjgrewal23 <purujeet.grewal23@gmail.com>
2020-04-17 21:49:09 -0600

    Testing and UI merged

--- app/src/androidTest/java/com/example/worker/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/com/example/worker/ExampleInstrumentedTest.java
@@ -2,6 +2,8 @@ package com.example.worker;
 
 
 import android.content.Context;
+import android.content.Intent;
+import android.view.KeyEvent;
 
 import org.junit.Before;
 import org.junit.Rule;
@@ -12,11 +14,18 @@ import androidx.test.rule.ActivityTestRule;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.junit.runner.notification.RunListener;
 
+import static android.app.PendingIntent.getActivity;
+import static androidx.core.content.ContextCompat.startActivity;
 import static androidx.test.espresso.Espresso.onView;
 import static androidx.test.espresso.action.ViewActions.click;
+import static androidx.test.espresso.action.ViewActions.swipeLeft;
+import static androidx.test.espresso.action.ViewActions.swipeRight;
 import static androidx.test.espresso.action.ViewActions.typeText;
+import static androidx.test.espresso.assertion.ViewAssertions.matches;
 import static androidx.test.espresso.matcher.ViewMatchers.withId;
+import static androidx.test.espresso.matcher.ViewMatchers.withText;
 import static org.junit.Assert.*;
 
 /**
@@ -30,7 +39,6 @@ public class ExampleInstrumentedTest {
     @Rule
     public ActivityTestRule<MainActivity> mActivityRule = new ActivityTestRule<>(MainActivity.class);
 
-
     @Test
     public void useAppContext() {
         // Context of the app under test.
@@ -40,39 +48,50 @@ public class ExampleInstrumentedTest {
     }
 
     @Test
-    public void testLogin() {
-
-
-        onView(withId(R.id.login)).perform(click());
-        onView(withId(R.id.email)).perform(typeText("jacobzenger@gmail.com"));
-        onView(withId(R.id.password)).perform(typeText("Testing1234"));
-        onView(withId(R.id.loginButton)).perform(click());
-
-        /* Go into settings and check that the name is bob */
-        onView(withId(R.id.settinsBtn)).perform(click());
-        assertEquals(onView(withId(R.id.name)), "bob");
+    public void testSwipe() {
+        onView(withId(R.id.frame)).perform(swipeLeft());
+        onView(withId(R.id.frame)).perform(swipeRight());
+        onView(withId(R.id.frame)).perform(swipeLeft());
+        onView(withId(R.id.frame)).perform(swipeRight());
+        onView(withId(R.id.frame)).perform(swipeLeft());
+        onView(withId(R.id.frame)).perform(swipeRight());
     }
 
     @Test
-    public void testRegister() {
-        onView(withId(R.id.register)).perform(click());
-        onView(withId(R.id.name)).perform(typeText("bob"));
-        onView(withId(R.id.email)).perform(typeText("jacobzenger@gmail.com"));
-        onView(withId(R.id.password)).perform(typeText("Testing1234"));
-        onView(withId(R.id.registerButton)).perform(click());
-
-        /* Go into settings and check that the name is bob */
-        onView(withId(R.id.settinsBtn)).perform(click());
-        assertEquals(onView(withId(R.id.name)), "bob");
-    }
-
-/*    @Test
-    public void testRightSwipe() {
-
+    public void testGoToMatches() {
+        onView(withId(R.id.matches)).perform(click());
+        onView(withId(R.id.recycler)).perform(click());
+        onView(withId(R.id.msg)).perform(typeText("How discrete is your service?"));
+        onView(withId(R.id.sendBtn)).perform(click());
+        onView(withId(R.id.msg)).perform(typeText("and how much do you charge to get sardine oil out of carpet?"));
+        onView(withId(R.id.sendBtn)).perform(click());
     }
 
     @Test
-    public void testLeftSwipe() {
+    public void ztestGoToSettings() {
+        onView(withId(R.id.settinsBtn)).perform(click());
+        onView(withId(R.id.description)).perform(typeText("Looking for new husband who loves sardine oil and tigers"));
+        onView(withId(R.id.phone)).perform(typeText("4205556969"));
+        onView(withId(R.id.name)).perform(typeText(" Baskin"));
+        onView(withId(R.id.name)).check(matches(withText("Carol Baskin")));
+    }
 
+    @Test
+    public void testSignOut() {
+        onView(withId(R.id.signOut)).perform(click());
+    }
+
+    /*
+    * Below is testing switching tasks for a worker
+    * Either the above tests can run or below ones but not both
+    *
+
+    @Test
+    public void testAddSetTask() {
+        onView(withId(R.id.settinsBtn)).perform(click());
+        onView(withId(R.id.editText3)).perform(typeText("Git Baskin"));
+        onView(withId(R.id.editText2)).perform(typeText("Git Tigers"));
+        onView(withId(R.id.editText)).perform(typeText("Git Money"));
     }*/
+
 }
